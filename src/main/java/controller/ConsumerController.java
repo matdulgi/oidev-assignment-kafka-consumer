759c37626c6714cf306bab696f247c394421eb50
yura <yrdog9494@naver.com>
2021-11-22 01:03:44 +0900

    1122

--- src/main/java/controller/ConsumerController.java
+++ src/main/java/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package controller;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -10,14 +11,24 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/kafka/consumer")
 public class ConsumerController {
+    ConsumerController(){
+        System.out.println("sd;oifjaodsifj");
+    }
 
 
 
 
     @RequestMapping("/config")
+    @ResponseBody
     public void config(HttpServletRequest req){
-
+        System.out.println("dfadsfdsaf");
     }
+
+    @RequestMapping("/index")
+    public String index(){
+       return "index";
+    }
+
 
 }
 
---
+++ src/main/java/controller/Inittest.java
@@ -0,0 +1,13 @@
+package controller;
+
+
+import javax.servlet.http.HttpServlet;
+
+public class Inittest extends HttpServlet {
+    @Override
+    public void init(){
+        System.out.println("testServlet" );
+
+    }
+
+}
